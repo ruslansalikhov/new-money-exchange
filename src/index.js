0b13c6a30c91e6f2cc957ecb06f219bf95fab12f
Ruslan Salikhov <ruslan.salihov@gmail.com>
2019-07-02 01:48:58 +0500

    straightforward solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    // edge case
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+
+    let change = {};
+
+    let coins = [[50, "H"], [25, "Q"], [10, "D"], [5, "N"], [1, "P"]];
+    coins.sort((a,b) => a[0] - b[0]); // be sure that order is correct
+
+    do {
+        let [value, sym] = coins.pop();
+        while (currency >= value) {
+            currency -= value;
+            change[sym] = 1 + (!!change[sym] ? change[sym] : 0);
+        }
+    } while (coins.length > 0);
+
+    return change;
 }
